2a44c811c4e5d329c8564b2efa22de98049919c3
Noa Burt <noaburt051221@gmail.com>
2024-08-22 01:41:06 +0100

    Install script successful, mkcc needs work but in working order

--- install.sh
+++ install.sh
@@ -3,17 +3,19 @@
 
 # A script to set up mkcc, clones repo to home dir and creates alias
 
-# curl -s GITHUBLINK/install.sh
-# chmod +x mkcc.sh (?)
+#shopt -s expand_aliases
 
-shopt -s expand_aliases
-
-gitlink="https://github.com/LINK/mkcc.sh"
+gitlink="https://github.com/noaburt/makeccproj/mkcc.sh"
 
 echo "Setting up mkcc [ version 0.0.1 ]"
+chmod +x ./mkcc.sh
 
-echo "Cloning mkcc files to home directory '$HOME'"
-curl -s $gitlink > ~/.mkcc.sh
+echo "Linking mkcc script to home directory '$HOME'"
+ln ./mkcc.sh ${HOME}/.mkcc.sh
+
 
 echo "Aliasing '${HOME}/.mkcc.sh' to mkcc"
 alias mkcc="${HOME}/.mkcc.sh"
+
+echo ""
+echo "Setup complete, run mkcc to see more info"
