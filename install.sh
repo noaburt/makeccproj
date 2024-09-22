3638229c280a822443e30e342f471fc63614347a
Noa Burt <noaburt051221@gmail.com>
2024-09-22 22:28:52 +0100

    install.sh working

--- install.sh
+++ install.sh
@@ -3,38 +3,33 @@
 
 # A script to set up mkcc; 
 
-scriptpath="$($HOME)/.mkcc/"
-gitversion="$(curl https://raw.githubusercontent.com/noaburt/mkcc/refactor/VERSION)"
+scriptpath="${HOME}/.mkcc"
+gitversion="$(curl -s https://raw.githubusercontent.com/noaburt/mkcc/refactor/VERSION)"
 
+echo ""
 
 if [[ $? -ne 0 ]]; then
     echo "Failed to fetch from git, exiting..." >&2
-    exit
+    return
 fi
 
 # Check if mkcc is already installed
 
 if [ -d "$scriptpath" ]; then
-    echo "Checking for updates for: mkcc version $version"
 
     # Check for updated version
     
-    currentv="$(cat ${scriptpath}/VERSION)"
-    if [ currentv == gitversion ]; then
+    read -r currentv < $scriptpath/VERSION
+    echo "Checking for updates for: mkcc version $currentv"
+    
+    if [ currentv = gitversion ]; then
 	echo "No new versions avaliable, exiting..." >&2
-	exit
+	return
     fi
-
-    # Confirm update
     
     echo "Version $gitversion avaliable"
-    read -p "Install mkcc version ${gitversion}? [y/n]: " confirm
-
-    if [[ $confirm == "n" ]] || [[ $confirm != "y" ]]; then
-	echo "Cancelling installation..."
-	exit
-    fi
-    
+    echo ""
+    rm -rf $scriptpath
 fi
 
 echo "Installing mkcc version $gitversion"
@@ -42,23 +37,26 @@ echo "Installing mkcc version $gitversion"
 
 # Install mkcc
 
-git clone https://github.com/noaburt/mkcc.git $scriptpath
+mkdir $scriptpath
+git clone -q https://github.com/noaburt/mkcc.git $scriptpath
 
 if [[ $? -ne 0 ]]; then
+    rm -rf $scriptpath
     echo "Git cloning failed, exiting..." >&2
-    exit
+    return
 fi
 
-make -C $scriptpath
+make -q -C $scriptpath
 
 if [[ $? -ne 0 ]]; then
+    rm -rf $scriptpath
     echo "Make failed, exiting..." >&2
-    exit
+    return
 fi
 
 
 echo ""
 echo "mkcc version $gitversion installed successfully"
-echo "add 'alias mkcc=${scriptpath}' to your .bashrc (or .zshrc if you use zshell)"
+echo "add 'alias mkcc=${scriptpath}/mkcc' to your .bashrc (or .zshrc if you use zshell)"
 echo "then run 'source .bashrc (or .zshrc)' to reload shell"
 echo "run 'mkcc -h / --help' for usage"
